1e34896ecc4f7ac3007a4e0255422bbaa4aeb121
Markez <marqezochy12@gmail.com>
2018-06-11 17:42:27 +0300

    updates

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,1 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from .models import Order
-# Register your models here.
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug']
-    prepopulated_fields = {'slug': ('order_number',)}
